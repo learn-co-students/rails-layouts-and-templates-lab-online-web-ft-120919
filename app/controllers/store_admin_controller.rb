451c1f1384e564feac61a692d6493380f937b852
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-11-02 21:48:23 -0600

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,15 @@
+class StoreAdminController < ApplicationController
+    render :layout => "admin"
+
+    def home
+    end
+
+    def orders
+        render :layout => "order_administration"
+    end
+
+    def invoice
+        render :layout => false
+    end
+
+end
